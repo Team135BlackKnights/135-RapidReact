55e4a85aa35114dbdc88dca6e9db4e7c1f449f33
JexPurx <jexpurx@gmail.com>
2022-01-15 10:08:33 -0500

    Hanging mechanism again

---
+++ src/main/java/frc/robot/commands/Hanging/Hanging.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.Hanging;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotMap.Hang;
+
+public class Hanging extends CommandBase{
+    private final Hang hang;
+    public Hanging(Hang subsystem) {
+        hang = subsystem;
+        addRequirements(subsystem);
+
+    }
+
+private void addRequirements(Hang subsystem) {
+    }
+public void initialize(){
+
+}
+public void execute() {
+}
+
+
+
+
+
+    
+}
