8c8d4d2514d8ef0d849df2f8ff1e0abcf77ebcce
JexPurx <jexpurx@gmail.com>
2022-01-15 10:27:17 -0500

    A

--- src/main/java/frc/robot/commands/Hanging/Hanging.java
+++ src/main/java/frc/robot/commands/Hanging/Hanging.java
@@ -1,10 +1,9 @@
 package frc.robot.commands.Hanging;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotMap.Hang;
-
+import frc.robot.subsystems.Hang;
 public class Hanging extends CommandBase{
-    private final Hang hang;
+    private final frc.robot.subsystems.Hang hang;
     public Hanging(Hang subsystem) {
         hang = subsystem;
         addRequirements(subsystem);
@@ -17,6 +16,8 @@ public void initialize(){
 
 }
 public void execute() {
+    hang.VerticalHang(0);
+    hang.HorHang(0);
 }
 
 
---
+++ src/main/java/frc/robot/subsystems/Hang.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
+    
+public class Hang extends SubsystemBase{
+    public CANSparkMax Vert1;
+    public CANSparkMax Vert2;
+    public CANSparkMax Hor1;
+    public CANSparkMax Hor2;
+
+    public Hang(){
+        CANSparkMax Vert1 = new CANSparkMax(RobotMap.Hang.V1_ID, MotorType.kBrushless);
+        CANSparkMax Vert2 = new CANSparkMax(RobotMap.Hang.V2_ID, MotorType.kBrushless);
+        CANSparkMax Hor1 = new CANSparkMax(RobotMap.Hang.H1_ID, MotorType.kBrushless);
+        CANSparkMax Hor2 = new CANSparkMax(RobotMap.Hang.H2_ID, MotorType.kBrushless);
+
+    }
+    public void VerticalHang(double power){
+        Vert1.set(power);
+        Vert2.set(power);
+    }
+    public void HorHang(double power){
+        Hor1.set(power);
+        Hor2.set(power);
+    }
+}
