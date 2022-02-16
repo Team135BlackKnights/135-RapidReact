f4c85c0dd2a606863bf143d957b03eaebfa70fe8
Gavin <robotics@phm.k12.in.us>
2022-02-16 18:59:45 -0500

    Update runShooter.java

--- src/main/java/frc/robot/commands/Turret/runShooter.java
+++ src/main/java/frc/robot/commands/Turret/runShooter.java
@@ -35,7 +35,7 @@ public class runShooter extends CommandBase {
     SmartDashboard.putNumber("RPM G", turret.shooter.getRate() * 60);
     SmartDashboard.putNumber("RPM", turret.shooter.getRate() * 60);
     
-    desired = limit(-RobotContainer.leftJoystick.getRawAxis(3), .8, 0); //change this num with testing
+    desired = limit(-RobotContainer.leftJoystick.getRawAxis(3), .8, 0); 
     error = desired - (turret.shooter.getRate() * 60) / 10000;
 
     iTop = desired * 1.34;
