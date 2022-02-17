dffcac037cc6ec6b8a5d601b6ff236b9cfd8ad1a
JexPurx <65154039+JexPurx@users.noreply.github.com>
2022-02-16 23:21:21 -0500

    Resolved hang motor conflictions
    Forgive me Father, for I have YandereDev'ed
    
    Needed to put all code that set the motor to anything into one statement so we don't have code conflicting with itself

--- src/main/java/frc/robot/commands/Hanging/Hanging.java
+++ src/main/java/frc/robot/commands/Hanging/Hanging.java
@@ -33,20 +33,27 @@ public class Hanging extends CommandBase {
          Hang.Vert1.set(0);
          Hang.Vert2.set(0);
         }
+        
         else if ((x==3300)&& (RobotContainer.hangStick.getRawAxis(1)<=-0.1)) {
             Hang.Vert1.set((RobotContainer.hangStick.getRawAxis(1)/2));
             Hang.Vert2.set((RobotContainer.hangStick.getRawAxis(1)/2));
             x = x-(1)*(RobotContainer.hangStick.getRawAxis(1));
           }
-        if ( x== -0.5) {
+          else if ( x== -0.5) {
           Hang.Vert1.set(0);
           Hang.Vert2.set(0);
         }
-        else if ((x==-0.5)&& (RobotContainer.hangStick.getRawAxis(1)>=.1)) {
+            else if ((x==-0.5)&& (RobotContainer.hangStick.getRawAxis(1)>=.1)) {
             Hang.Vert1.set((RobotContainer.hangStick.getRawAxis(1)/2));
             Hang.Vert2.set((RobotContainer.hangStick.getRawAxis(1)/2));
             x = x+(1)*(RobotContainer.hangStick.getRawAxis(1));
           }
+        else {
+        Hang.Vert1.set((RobotContainer.hangStick.getRawAxis(1))/2);
+        Hang.Vert2.set((RobotContainer.hangStick.getRawAxis(1))/2);
+        }
+
+
 
     
 
