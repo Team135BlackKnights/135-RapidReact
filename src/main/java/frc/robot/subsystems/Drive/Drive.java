e8e6fbe1264effcf64044f1f94562a916a511e07
Ttemeles <91227969+Ttemeles@users.noreply.github.com>
2022-02-19 16:39:24 -0500

    Update RobotMap

--- src/main/java/frc/robot/subsystems/Drive/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive/Drive.java
@@ -7,8 +7,10 @@ package frc.robot.subsystems.Drive;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -16,57 +18,73 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class Drive extends SubsystemBase {
-  public DifferentialDrive tank;
-  public AHRS navx;
-  CANSparkMax FrontLeft = new CANSparkMax(RobotMap.Drive.FL_ID, MotorType.kBrushless);
-  CANSparkMax FrontRight = new CANSparkMax(RobotMap.Drive.FR_ID, MotorType.kBrushless);
-  CANSparkMax BackLeft = new CANSparkMax(RobotMap.Drive.BL_ID, MotorType.kBrushless);
-  CANSparkMax BackRight = new CANSparkMax(RobotMap.Drive.BR_ID, MotorType.kBrushless);
+    public DifferentialDrive tank;
+    public AHRS navx;
+    CANSparkMax FrontLeft = new CANSparkMax(RobotMap.Drive.FL_ID, MotorType.kBrushless);
+    CANSparkMax FrontRight = new CANSparkMax(RobotMap.Drive.FR_ID, MotorType.kBrushless);
+    CANSparkMax BackLeft = new CANSparkMax(RobotMap.Drive.BL_ID, MotorType.kBrushless);
+    CANSparkMax BackRight = new CANSparkMax(RobotMap.Drive.BR_ID, MotorType.kBrushless);
+    public RelativeEncoder e_FrontLeft, e_FrontRight, e_BackLeft, e_BackRight;
 
-  public RelativeEncoder e_FrontLeft, e_FrontRight, e_BackLeft, e_BackRight;
+    Timer timer = new Timer();
 
-  /** Creates a new Drive. */
-  public Drive() {
-    
-    FrontLeft.enableVoltageCompensation(12);
-    FrontRight.enableVoltageCompensation(12);
-    BackLeft.enableVoltageCompensation(12);
-    BackRight.enableVoltageCompensation(12); 
-    
-    e_FrontLeft = FrontLeft.getEncoder();
-    e_FrontRight = FrontRight.getEncoder();
-    e_BackLeft = BackLeft.getEncoder();
-    e_BackRight = BackRight.getEncoder();
+    /** Creates a new Drive. */
+    public Drive() {
+
+        
+
+        FrontLeft.enableVoltageCompensation(12);
+        FrontRight.enableVoltageCompensation(12);
+        BackLeft.enableVoltageCompensation(12);
+        BackRight.enableVoltageCompensation(12);
+
+        e_FrontLeft = FrontLeft.getEncoder();
+        e_FrontRight = FrontRight.getEncoder();
+        e_BackLeft = BackLeft.getEncoder();
+        e_BackRight = BackRight.getEncoder();
 
 
-    MotorControllerGroup left = new MotorControllerGroup(FrontLeft, BackLeft);
-    MotorControllerGroup right = new MotorControllerGroup(FrontRight, BackRight);
+        MotorControllerGroup left = new MotorControllerGroup(FrontLeft, BackLeft);
+        MotorControllerGroup right = new MotorControllerGroup(FrontRight, BackRight);
 
-    tank = new DifferentialDrive(left, right);
+        tank = new DifferentialDrive(left, right);
 
-    // Declares a new Navx and immediately sets it to 0
-    //navx = new AHRS(RobotMap.Drive.navXPort);
-  }
+        // Declares a new Navx and immediately sets it to 0
+        //navx = new AHRS(RobotMap.Drive.navXPort);
+    }
 
-  public void tankDrive(double left, double right){
-    tank.tankDrive(left, right);
-  }
+    public void tankDrive(double left, double right) {
+        tank.tankDrive(left, right);
+    }
 
-  public void resetEncoders() {
-    e_FrontLeft.setPosition(0);
-    e_FrontRight.setPosition(0);
-    e_BackLeft.setPosition(0);
-    e_BackRight.setPosition(0);
-  }
+    public float navXCorrectOffset(){
+        //math from here: https://www.chiefdelphi.com/t/off-centering-a-gyro/380703/7
+        Float x = navx.getQuaternionX() / (float) timer.get(); //distance of navx per second
+        x = (float) (x * Math.pow(Math.PI, 2) * 5.67);          //Area of Navx
+        return (float) (.2 * x * timer.get());                  //multiply by .2 and then convert to just degrees                    
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+    public void resetEncoders() {
+        e_FrontLeft.setPosition(0);
+        e_FrontRight.setPosition(0);
+        e_BackLeft.setPosition(0);
+        e_BackRight.setPosition(0);
+    }
 
-    SmartDashboard.putNumber("Left Power",(FrontLeft.get() + BackLeft.get() )/ 2);
-    SmartDashboard.putNumber("Right Power",(FrontRight.get() + BackRight.get() )/ 2);
-    //output side power
-    
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        if (navx.isRotating())
+            timer.start();
+        else {
+            timer.stop();
+            timer.reset();
+        }
 
-  }
+        SmartDashboard.putNumber("Left Power", (FrontLeft.get() + BackLeft.get()) / 2);
+        SmartDashboard.putNumber("Right Power", (FrontRight.get() + BackRight.get()) / 2);
+        //output side power
+
+
+    }
 }
