1a2b4aa5e0cad03ac490625a8399d4c75ce5251e
Ttemeles <91227969+Ttemeles@users.noreply.github.com>
2022-03-30 15:48:24 -0400

    Matsuri Here
    Added brake mode for drivers on left joystick

--- src/main/java/frc/robot/commands/Drive/tankDrive.java
+++ src/main/java/frc/robot/commands/Drive/tankDrive.java
@@ -36,6 +36,19 @@ public void initialize() {
   public void execute() {
     double leftSpeed =   RobotContainer.leftJoystick.getRawAxis(RobotMap.KOI.VERTICAL_AXIS) * (-RobotContainer.leftJoystick.getRawAxis(3) + 1) / 2;
     double rightSpeed = -RobotContainer.rightJoystick.getRawAxis(RobotMap.KOI.VERTICAL_AXIS) * (-RobotContainer.leftJoystick.getRawAxis(3) + 1) / 2;  
+    
+    if (-RobotContainer.leftJoystick.getRawAxis(3)==0) {
+      drive.FrontLeft.setIdleMode(IdleMode.kBrake);
+      drive.FrontRight.setIdleMode(IdleMode.kBrake);
+      drive.BackLeft.setIdleMode(IdleMode.kBrake);
+      drive.BackRight.setIdleMode(IdleMode.kBrake);
+    }
+    else {
+      drive.FrontLeft.setIdleMode(IdleMode.kCoast);
+      drive.FrontRight.setIdleMode(IdleMode.kCoast);
+      drive.BackLeft.setIdleMode(IdleMode.kCoast);
+      drive.BackRight.setIdleMode(IdleMode.kCoast); 
+    }
 
     drive.tankDrive(-leftSpeed, -rightSpeed);
   }
